f388342d21fbddc1ca763d9463c116fbc9255572
Init101 <40778008+Init101@users.noreply.github.com>
2019-12-12 15:05:43 +1100

    Wheelchair Body Model

---
+++ NeuroNode Wheelchair VR/Assets/Scripts/target_Course.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class target_Course : MonoBehaviour
+{
+    public bool Goal = false;
+    public float time;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        Goal = true;
+    }
+
+    private void Update()
+    {
+        if(Goal == true)
+        {
+            // Goal Audio Trigger Goes Here
+            Debug.Log("FINISHED THE TARGET COURSE!!!");
+
+            //Start Count Down to return to Menu
+            time += Time.deltaTime;
+        }
+
+        if(time >= 10.0f)
+        {
+         //Scene Switcher go here when we have a Main Menu
+         Debug.Log("Back to Menu");
+        }        
+    }
+}
