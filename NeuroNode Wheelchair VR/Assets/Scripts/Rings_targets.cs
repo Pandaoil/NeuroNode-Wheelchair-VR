f823d06737ae1e92480ec5ff2aeeba4118e18fab
JKinoial <40745595+JKinoial15@users.noreply.github.com>
2019-12-17 02:29:29 +1100

    ground asset layout and ring course

---
+++ NeuroNode Wheelchair VR/Assets/Scripts/Rings_targets.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rings_targets : MonoBehaviour
+{
+    public GameObject ringManager;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void OnTriggerEnter(Collider other)
+    {
+        ringManager.GetComponent<ring_Course>().NextRing();
+        gameObject.GetComponent<BoxCollider>().enabled = false;
+    }
+}
---
+++ NeuroNode Wheelchair VR/Assets/Scripts/ring_Course.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ring_Course : MonoBehaviour
+{
+    public GameObject[] nextPoint;
+    public int ringcheck = 0;
+    public float time;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(ringcheck == nextPoint.Length)
+        {
+
+            // Goal Audio Trigger Goes Here
+            Debug.Log("FINISHED THE RING COURSE!!!");
+
+            //Start Count Down to return to Menu
+            time += Time.deltaTime;
+            ringcheck++;
+        }
+
+        if (time >= 10.0f)
+        {
+            //Scene Switcher go here when we have a Main Menu
+            Debug.Log("Back to Menu");
+        }
+    }
+
+    public void NextRing()
+    {
+        // check if there is a next ring in course
+        if (ringcheck <= nextPoint.Length)
+        {
+            Debug.Log("next");
+
+            // Turns off current ring in course
+            nextPoint[ringcheck].SetActive(false);
+
+            // Increase ring counter
+            ringcheck++;
+
+            if (ringcheck < nextPoint.Length)
+            {
+                // Turns on next ring in course
+                nextPoint[ringcheck].SetActive(true);
+
+            }
+            Debug.Log(nextPoint.Length);
+
+        }
+       
+        
+    }
+}
