a7a53713f8478c947d13a8392e450365a0e64512
JKinoial <40745595+JKinoial15@users.noreply.github.com>
2019-12-30 20:13:44 +1100

    Simple Menu

---
+++ NeuroNode Wheelchair VR/Assets/Scripts/Menu_Navigation.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Menu_Navigation : MonoBehaviour
+{
+    public string theLevel;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+
+    // this is for the tradtional VR control that require touch
+    public void OnTriggerEnter(Collider other)
+    {
+        SceneManager.LoadScene(theLevel);
+    }
+}
